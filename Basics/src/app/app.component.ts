0fc2e28026c9c43d4602dda4f758b0e7f4212735
Abel <abelzeleke5173@gmail.com>
2023-03-16 12:43:50 +0300

    first hello world Angular app

--- Basics/src/app/app.component.ts
+++ Basics/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Hello World!</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: ['color: "red"']
 })
 export class AppComponent {
-  title = 'Basics';
+  name = 'Luis';
+  imgURL = 'https://thumbs.dreamstime.com/b/happy-golden-retriever-puppy-seven-week-old-outdoors-sunny-day-46046484.jpg';
+  changeImage(e: KeyboardEvent){
+    this.imgURL = (e.target as HTMLInputElement).value
+  }
+  getName(){
+    return this.name;
+  }
 }
