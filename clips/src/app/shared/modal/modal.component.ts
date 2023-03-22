ec8758115e36c63d16c0382075421b5ae5dd96bf
Abel <abelzeleke5173@gmail.com>
2023-03-22 10:24:37 +0300

    added modal services

--- clips/src/app/shared/modal/modal.component.ts
+++ clips/src/app/shared/modal/modal.component.ts
@@ -8,6 +8,9 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent {
   constructor(public modal: ModalService) {
-    console.log(this.modal.visible);    
+
+  }
+  closeModal(){
+    this.modal.toggleModal()
   }
 }
